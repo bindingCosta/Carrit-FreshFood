35ca935fac95e5e243ba6c468f6ca9317a4db2c3
marcelo.costa <costanetomj@gmail.com>
2024-12-20 16:44:07 -0300

    App Architectury

---
+++ Carrit/Core/Home/Views/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Carrit
+//
+//  Created by Marcelo Costa on 20/12/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    Home()
+}
