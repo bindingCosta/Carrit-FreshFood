e16a9beb77b0867b38ab469b3bba71a766b56a9d
marcelo.costa <costanetomj@gmail.com>
2024-12-20 16:33:15 -0300

    Initial Commit

---
+++ Carrit/CarritApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarritApp.swift
+//  Carrit
+//
+//  Created by Marcelo Costa on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CarritApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
